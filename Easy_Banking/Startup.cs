24699ada7f2a4c9f0686d5aea518be091cd17a6c
Alexander <atvahid@hotmail.com>
2019-02-11 19:21:54 -0500

    Add project files.

---
+++ Easy_Banking/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Easy_Banking.Startup))]
+namespace Easy_Banking
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
